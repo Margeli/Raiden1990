4f7624056c52b42e866d669d1937325b868a0483
Margeli <niniopantalones@gmail.com>
2017-04-16 18:08:37 +0200

    ...

--- Raiden Game/BonusSpaceship.cpp
+++ Raiden Game/BonusSpaceship.cpp
@@ -5,14 +5,14 @@
 Bonus_Spaceship::Bonus_Spaceship (int x, int y) : Enemy(x, y) 
 {
 	fly.PushBack({ 45,43,59,50 });
-	fly.PushBack({ 116,43,59,50 });
+	fly.PushBack({ 115,43,59,50 });
 	fly.PushBack({ 184,43,59,50 });
-	fly.PushBack({ 252,43,59,50 });
+	fly.PushBack({ 253,43,59,50 });
 	fly.PushBack({ 319,43,59,50 });
 	animation = &fly;
 
 	
-	collider = App->collision->AddCollider({ 0, 0, 35, 28 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
+	collider = App->collision->AddCollider({ 0, 0, 59, 50 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
 
 }
 
