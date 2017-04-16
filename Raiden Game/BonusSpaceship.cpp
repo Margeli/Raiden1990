b72006d7f7bb23494829df45825b25942c5e1725
Vladislav Bulantsev <vladislavbv96@gmail.com>
2017-04-16 17:49:58 +0200

    enemy animation bug

--- Raiden Game/BonusSpaceship.cpp
+++ Raiden Game/BonusSpaceship.cpp
@@ -4,11 +4,11 @@
 
 Bonus_Spaceship::Bonus_Spaceship (int x, int y) : Enemy(x, y) 
 {
-	fly.PushBack({ 6,1,35,28 });
-	fly.PushBack({ 6,1,35,28 });
-	fly.PushBack({ 6,1,35,28 });
-	fly.PushBack({ 6,1,35,28 });
-	fly.PushBack({ 6,1,35,28 });
+	fly.PushBack({ 51,53,114,95 });
+	fly.PushBack({ 185,53,35,28 });
+	fly.PushBack({ 316,53,35,28 });
+	fly.PushBack({ 445,53,35,28 });
+	fly.PushBack({ 571,53,35,28 });
 	animation = &fly;
 
 	
