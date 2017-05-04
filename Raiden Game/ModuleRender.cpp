7a6d6f121900bf9d0b1cac3ab2b47a2f50a9bd6d
Vladislav Bulantsev <vladislavbv96@gmail.com>
2017-05-04 10:05:57 +0200

    fullscreen mode
    peta si apretas ESC

--- Raiden Game/ModuleRender.cpp
+++ Raiden Game/ModuleRender.cpp
@@ -31,12 +31,15 @@ bool ModuleRender::Init()
 	}
 
 	renderer = SDL_CreateRenderer(App->window->window, -1, flags);
-	
+
+
 	if(renderer == NULL)
 	{
 		LOG("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
 		ret = false;
 	}
+
+	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	return ret;
 }
