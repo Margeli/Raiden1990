b6ab9bfb31e4cdb2ca95c24cfbe1415cb1b0c336
Margeli <niniopantalones@gmail.com>
2017-05-04 10:20:57 +0200

    trying to add controller

--- Raiden Game/ModuleInput.cpp
+++ Raiden Game/ModuleInput.cpp
@@ -25,6 +25,23 @@ bool ModuleInput::Init()
 		LOG("SDL_EVENTS could not initialize! SDL_Error: %s\n", SDL_GetError());
 		ret = false;
 	}
+	if (SDL_Init(SDL_INIT_JOYSTICK)< 0) {
+		LOG("SDL_INIT_JOYSTICK could not initialize , SDL Error: %s\n", SDL_GetError() )
+	}
+	if (SDL_NumJoysticks() < 1)	{
+		LOG(" No joysticks connected!\n");
+	}
+	else{
+		//Load joystick
+		controller = SDL_GameControllerOpen(controller_index);
+		use_controller = true;
+		if (controller == NULL){
+			LOG(" Unable to open game controller! SDL Error: %s\n", SDL_GetError());
+			use_controller = false;
+		}
+	}
+	
+
 
 	return ret;
 }
@@ -57,12 +74,19 @@ update_status ModuleInput::PreUpdate()
 	if (keyboard[SDL_SCANCODE_ESCAPE])
 		return update_status::UPDATE_STOP;
 
+
+
+
+
 	return update_status::UPDATE_CONTINUE;
 }
 
 // Called before quitting
 bool ModuleInput::CleanUp()
 {
+	//Close game controller
+	SDL_GameControllerClose(controller);
+	controller = NULL;
 	LOG("Quitting SDL input event subsystem");
 	SDL_QuitSubSystem(SDL_INIT_EVENTS);
 	return true;
--- Raiden Game/ModuleInput.h
+++ Raiden Game/ModuleInput.h
@@ -4,8 +4,11 @@
 #include "Module.h"
 #include "Globals.h"
 #include "SDL\include\SDL_scancode.h"
+#include "SDL\include\SDL_gamecontroller.h"
 
 #define MAX_KEYS 300
+#define JOYSTICK_DEAD_ZONE 8000
+
 
 enum KEY_STATE
 {
@@ -28,6 +31,16 @@ public:
 
 public:
 	KEY_STATE keyboard[MAX_KEYS];
+	SDL_GameController* controller = NULL;
+
+public:
+	/*SDL_Event e;
+
+	//Normalized direction
+	int xDir = 0;
+	int yDir = 0;*/
+	bool use_controller = false;
+	int controller_index = 0;
 };
 
 #endif // __ModuleInput_H__
