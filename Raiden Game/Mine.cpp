32ac48d69be4c8c09ba15b710e21220df16e849a
Margeli <niniopantalones@gmail.com>
2017-05-09 16:27:31 +0200

    bis

---
+++ Raiden Game/Mine.cpp
@@ -0,0 +1,28 @@
+#include "Application.h"
+#include "Mine.h"
+#include "ModuleCollision.h"
+#include "ModuleEnemies.h"
+#include "ModuleParticles.h"
+#include "ModulePlayer.h"
+#include "ModulePlayer2.h"
+#include "ModuleTextures.h"
+#include "ModuleAudio.h"
+
+
+Mine::Mine(int x, int y, int shoot_num) : Enemy(x, y)
+{
+	
+}
+
+void Mine::Move() {
+	
+
+}
+
+
+
+
+void Mine::OnCollision(Collider*collider, int num_enemy) {
+	
+
+}
--- Raiden Game/Mine.h
+++ Raiden Game/Mine.h
@@ -1,1 +1,41 @@
-#pragma once
+#ifndef __MINE_H__
+#define __MINE_H__
+
+#include "Enemy.h"
+#include "ModuleParticles.h"
+
+class Mine : public Enemy
+{
+private:
+	
+
+	Animation idle;
+	Particle explosion;
+	Mix_Chunk* fx_shoot = nullptr;
+	void OnCollision(Collider* collider, int num_enemy);
+
+
+	float speed;
+	float increment_y;
+	float initial_y;
+	
+	
+	float hits_life;
+	int shoot_number = 0;
+	uint score_points;
+
+
+
+
+
+public:
+
+	Mine(int x, int y, int shoot_num);
+	void Move();
+
+
+	
+
+};
+
+#endif// __MINE_H__
